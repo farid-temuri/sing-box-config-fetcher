cc51718389098d87ec03b2ed00dc49a698330f3e
Farid <56141667+farid-temuri@users.noreply.github.com>
2024-06-02 23:28:41 +0300

    wip

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/sh  
 
-SECONDS_TO_SLEEP=1
-CONFIG_URL=localhost:3000/config?asd=ass
+SECONDS_TO_SLEEP=3600
+CONFIG_URL=localhost:3000/config/ne?asd=ass
 
-while true  
-do  
+while true
+do
 	REQUESTED=`curl $CONFIG_URL`
 	echo $REQUESTED > ./sing-box/config.json
-	echo $REQUESTED
+	`docker-compose down`
+	`docker-compose up`
   sleep $SECONDS_TO_SLEEP
 done
