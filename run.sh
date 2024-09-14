108b932ed04498339507c4d3e4b014bf667a0575
Farid <56141667+farid-temuri@users.noreply.github.com>
2024-09-14 15:10:48 +0300

    feat: rm nohup

--- run.sh
+++ run.sh
@@ -1,11 +1,2 @@
-#!/bin/sh  
-. ./.env
-
-while true
-do
-	REQUESTED=`curl $CONFIG_URL`
-	echo $REQUESTED > ./sing-box/config.json
-	`docker compose down`
-	`docker compose up -d`
-  sleep $DELAY_IN_SECONDS
-done
+REQUESTED=`curl $CONFIG_URL`
+echo $REQUESTED > ./sing-box/config.json
