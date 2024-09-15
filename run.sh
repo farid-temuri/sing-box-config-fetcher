2fbce523ae15dd615c01e99c09458911779e4084
Farid <56141667+farid-temuri@users.noreply.github.com>
2024-09-15 15:17:01 +0300

    wip

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 
 while true
 do
-        REQUESTED=`curl $CONFIG_URL`
-        echo $REQUESTED > ./sing-box/config.json
-        `docker compose restart`
+	REQUESTED=`curl $CONFIG_URL`
+	echo $REQUESTED > ./sing-box/config.json
+	`docker compose restart`
   sleep $DELAY_IN_SECONDS
 done
